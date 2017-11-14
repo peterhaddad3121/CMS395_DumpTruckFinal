df13cdcb7ffa89380ddc9ac0d5be3d7875cbc85c
peter <peter@DESKTOP-IRHA0UC>
2017-11-14 15:41:25 -0600

    Added Variables

--- src/Simulation.java
+++ src/Simulation.java
@@ -1,5 +1,15 @@
+import generalSimulation.Distribution;
 
 public class Simulation {
+	
+	private Distribution travelTimes;
+	private Distribution weighingTime;
+	private Distribution loadingTime;
+	
+	private int callsComplete = 0;
+	private double clock = 0;
+	private int numberOfCalls = 1000;
+	
 
 	//Simulate for 1,000
 	
